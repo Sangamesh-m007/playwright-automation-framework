fb95f18e4a0b6301e00f983f06ced903130435ef
Sangamesh <mudakannavarsangamesh@gmail.com>
2025-04-10 14:49:41 +0530

    jai ram shree ram

--- playwright.config.ts
+++ playwright.config.ts
@@ -21,12 +21,7 @@ export default defineConfig({
   workers: process.env.CI ? 1 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
  // reporter: [["html"],['junit', { outputFile: 'results.xml' }]],
- //reporter: [['list'],['junit', { outputFile: 'results.xml' }]],
- reporter: [
-  ['list'],
-  ['junit', { outputFile: 'results.xml' }],
-  ['json', { outputFile: 'test-results.json' }],
-],
+ reporter: [['list'],['junit', { outputFile: 'results.xml' }]],
   // reporter: 'list',
   //reporter: 'line',
   //reporter: 'dot',
