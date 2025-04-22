2d1318a3f4bff0da2a505cf31ccc3822c023acf3
Sangamesh <mudakannavarsangamesh@gmail.com>
2025-04-22 10:34:29 +0530

    changes done

--- basic2/softstaking.spec.js
+++ basic2/softstaking.spec.js
@@ -12,7 +12,7 @@ const soft = await page.locator("//p[normalize-space()='Soft Staking']").textCon
     const stakeexplorebutton = await stakeexplore.textContent()
     expect(stakeexplorebutton).toBe("Explore")
     const video2 = await page.locator("(//video)[2]")
-    expect(video2).toBeVisible()
+    expect(video2).toBeVisible();
     const softtext = await page.getByText("Unlock effortless rewards with our Soft Staking Pool—flexible, straightforward, and designed to work for you.").innerText()
     console.log(softtext);
     expect(softtext).toBe("nlock effortless rewards with our Soft Staking Pool—flexible, straightforward, and designed to work for you.");
