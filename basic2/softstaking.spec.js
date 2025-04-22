914940ac58a90cff73617ea6b1cce27e82f1caa1
Sangamesh <mudakannavarsangamesh@gmail.com>
2025-04-22 10:49:13 +0530

    changes

--- basic2/softstaking.spec.js
+++ basic2/softstaking.spec.js
@@ -27,7 +27,7 @@ const soft = await page.locator("//p[normalize-space()='Soft Staking']").textCon
         const belowheading = await page.locator("p[class='lg:text-sm text-gray-40 text-sm md:text-lg max-w-[400px] lg:max-w-[450px] mx-auto transition-transform duration-700 will-change-transform translate-y-0']").textContent();
         expect(belowheading).toBe("Where $ADS holders innovate, earn rewards and shape digital advertising's future.");
         const button = await page.locator("button[type='button']")
-        expect(button).toBeVisible()
+        expect(button).toBeVisible();
         const buttontext = await button.textContent();
         expect(buttontext).toBe("Buy $ADS")
         const rewards = await page.locator("a[class='inline-flex gap-1 items-center justify-center whitespace-nowrap rounded-button ring-offset-background focus-visible:outline-none transition-all disabled:pointer-events-none disabled:opacity-20 underline underline-offset-2 text-gray-40 active:text-white hover:text-white lg:text-sm px-[48px] py-[10px] text-sm']").textContent()
